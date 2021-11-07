dc78adc68711524dad6419dcbf0a3a71f5f25ca6
fahadalduaij <fahad_osama@hotmail.com>
2021-11-07 21:20:39 +0300

    Done with User Store

--- src/stores/User.js
+++ src/stores/User.js
@@ -1,3 +1,69 @@
 import decode from "jwt-decode";
-import { makeObservable, observable, makeObservable } from "mobx";
+import { makeObservable, observable, makeObservable, action } from "mobx";
 import instance from "./instance";
+
+class UserData {
+	user = null;
+	signed = false;
+
+	constructor() {
+		makeObservable(this, {
+			user: observable,
+			signIn: action,
+			signUp: action,
+			signOut: action,
+		});
+	}
+
+	setUser = (token) => {
+		try {
+			localStorage.setItem("myToken", token);
+			instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+			this.user = decode(token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	checkForToken = () => {
+		const token = localStorage.getItem("myToken");
+		if (token) {
+			let tempUser = decode(token);
+			if (tempUser.exp > Date.now()) {
+				return this.setUser(token);
+			} else {
+				return this.signOut();
+			}
+		}
+	};
+
+	signIn = async (userInfo) => {
+		try {
+			const res = instance.post("/signin", userInfo);
+			this.setUser(res.data.token);
+			this.signed = true;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	signUp = async (userInfo) => {
+		try {
+			const res = instance.post("/signup", userInfo);
+			this.setUser(res.data.token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	signOut = () => {
+		delete instance.defaults.headers.common.Authorization;
+		localStorage.removeItem("myToken");
+		this.user = null;
+		this.signed = false;
+	};
+}
+
+const userData = new UserData();
+userData.checkForToken();
+export default userData;
