dd3c33f49ee2a6cb9222d12a9da3d04af56605cf
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-11-08 18:51:12 +0300

    css

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,14 @@ import movieData from "../stores/MovieData";
 import { observer } from "mobx-react";
 
 function MovieList() {
-	const movieList = movieData.movies.map((movie) => (
-		<MovieCard key={movie._id} movie={movie} />
-	));
-	return <div>{movieList}</div>;
+  const movieList = movieData.movies.map((movie) => (
+    <MovieCard key={movie._id} movie={movie} />
+  ));
+  return (
+    <div className="container-lg row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+      {movieList}
+    </div>
+  );
 }
 
 export default observer(MovieList);
