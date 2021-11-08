b36c9db902eafbd7c05ede64f1d971829dc42639
fahadalduaij <fahad_osama@hotmail.com>
2021-11-08 16:44:44 +0300

    adding function

--- src/stores/MovieData.js
+++ src/stores/MovieData.js
@@ -18,6 +18,22 @@ class MovieData {
 			console.log(error);
 		}
 	};
+
+	createMovie = async (movieInfo) => {
+		try {
+			if (!userData.user.isAdmin === true) {
+				return console.log("You are not admin");
+			} else {
+				const formData = new FormData();
+				for (const key in movieInfo) {
+					formData.append(key, movieInfo[key]);
+				}
+				const res = await instance.post("", formData);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 }
 
 const movieData = new MovieData();
