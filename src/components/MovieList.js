cf2c9f7e786b643bf4e2331c77aba84b07ebb55b
fahadalduaij <fahad_osama@hotmail.com>
2021-11-08 18:23:40 +0300

    adding re useable SearchBar

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,13 +3,17 @@ import MovieCard from "./MovieCard";
 import movieData from "../stores/MovieData";
 import { observer } from "mobx-react";
 import MovieModal from "./MovieModal";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function MovieList() {
-	const movieList = movieData.movies.map((movie) => (
-		<MovieCard key={movie._id} movie={movie} />
-	));
+	const [query, setQuery] = useState("");
+	const movieList = movieData.movies
+		.filter((movie) => movie.title.toLowerCase().includes(query.toLowerCase()))
+		.map((movie) => <MovieCard key={movie._id} movie={movie} />);
 	return (
 		<div>
+			<SearchBar setitem={setQuery} />
 			<MovieModal />
 			{movieList}
 		</div>
