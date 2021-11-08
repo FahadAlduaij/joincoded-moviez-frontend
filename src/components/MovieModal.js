6bae1b639a67bf2e7c67ef7d82e1a66f8018d9f2
Alkandari-Y <yayalkandari@gmail.com>
2021-11-08 22:54:45 +0300

    added multi select drop down but its not fully functional

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React  from "react";
 import { Form, Button, Container, Modal } from "react-bootstrap";
+import { observer } from "mobx-react";
 import { useState } from "react";
 import movieData from "../stores/MovieData";
+import genreData from "../stores/GenreData";
+import { default as ReactSelect } from "react-select";
+import GenreOptionsMovieCreate from "./GenreOptionsMovieCreate"
+
 
 function MovieModal() {
 	const [movie, setMovie] = useState({
 		title: "",
 		image: "",
 		releaseDate: "",
-		genres: "",
+		genres: [],
 		celebrities: "",
 	});
+
+	const [genresSelected, setGenresSelect] = useState({
+		optionSelected: null
+	})
+
+	const genreOptions = [];
+	genreData.genres.forEach(genre => {
+		genreOptions.push({value: genre.genreName, label: genre.genreName})
+	})
+
 
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
@@ -22,7 +37,17 @@ function MovieModal() {
 		setMovie({ ...movie, image: event.target.files[0] });
 	};
 
+	const reactSelectHandleChange = (selected) => {
+		setGenresSelect({
+			optionSelected: selected
+		});
+	};
+
 	const handleSubmit = (event) => {
+		const genreArray = []
+		genresSelected.optionSelected.forEach(element => genreArray.push(element.value))
+		console.log(genreArray)
+		setMovie({ ...movie, genres: genreArray })
 		event.preventDefault();
 		movieData.createMovie(movie);
 		handleClose();
@@ -71,13 +96,27 @@ function MovieModal() {
 
 							<Form.Group className="mb-3" controlId="formBasicPassword">
 								<Form.Label>Genres</Form.Label>
-								<Form.Control
+								{/* <Form.Control
 									onChange={handleChange}
 									name="genres"
 									value={movie.genres}
 									type="text"
 									placeholder="Enter genre"
-								/>
+								/> */}
+								<span>
+									<ReactSelect
+									options={genreOptions}
+									isMulti
+									closeMenuOnSelect={false}
+									hideSelectedOptions={false}
+									components={{
+										GenreOptionsMovieCreate
+									}}
+									onChange={reactSelectHandleChange}
+									allowSelectAll={true}
+									value={genresSelected.optionSelected}
+									/>
+								</span>
 							</Form.Group>
 
 							<Form.Group className="mb-3" controlId="formBasicPassword">
@@ -103,4 +142,4 @@ function MovieModal() {
 	);
 }
 
-export default MovieModal;
+export default observer(MovieModal);
