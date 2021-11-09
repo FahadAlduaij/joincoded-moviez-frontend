cfe50a94b347e7bb79b2c3c9b32f3f1047cdb0cc
fahadalduaij <fahad_osama@hotmail.com>
2021-11-10 00:01:43 +0300

    creating MoviesGenre

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,39 +3,28 @@ import { observer } from "mobx-react";
 import { useState } from "react";
 
 // Components
-import MovieCard from "./MovieCard";
 import MovieModal from "./MovieModal";
 import SearchBar from "./SearchBar";
+import MoviesGenre from "./MoviesGenre";
 
 // Stores
-import movieStore from "../stores/movieStore";
-import GenreModal from "./GenreModal";
+import genreStore from "../stores/genreStore";
 
 function MovieList() {
 	const [query, setQuery] = useState("");
 
-	const movieList = movieStore.movies
-		.filter((movie) => movie.title.toLowerCase().includes(query.toLowerCase()))
-		.map((movie) => <MovieCard key={movie._id} movie={movie} />);
+	const movieList = genreStore.genres.map((genre) => (
+		<MoviesGenre key={genre._id} genre={genre} query={query} />
+	));
 
 	return (
 		<div>
 			<div>
 				<MovieModal />
-				<GenreModal />
 				<SearchBar setItem={setQuery} />
 			</div>
 			<div className="mt-4 ">
-				<h1>Action</h1>
-				<div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
-					{movieList}
-				</div>
-			</div>
-			<div className="mt-4 ">
-				<h1 className="mt-4 ">Horror</h1>
-				<div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
-					{movieList}
-				</div>
+				<div>{movieList}</div>
 			</div>
 			{/* <div className="container-lg row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
 				{movieList}
---
+++ src/components/MoviesGenre.js
@@ -0,0 +1,19 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+function MoviesGenre({ genre, query }) {
+	const movies = genre.movies
+		.filter((movie) => movie.title.toLowerCase().includes(query.toLowerCase()))
+		.map((movie) => <MovieCard key={movie._id} movie={movie} />);
+
+	return (
+		<div>
+			<h1>{genre.name}</h1>
+			<div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
+				{movies}
+			</div>
+		</div>
+	);
+}
+
+export default MoviesGenre;
