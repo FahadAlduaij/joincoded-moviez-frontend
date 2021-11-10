7d51059a313346065cce5b0e62d6d5f25b7bb4ac
Alkandari-Y <yayalkandari@gmail.com>
2021-11-10 17:29:17 +0300

    testing singleStore working

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -7,31 +7,34 @@ import { Navigate } from "react-router-dom";
 import Tags from "./Tags";
 
 // stores
-import movieStore from "../stores/movieStore";
+// import movieStore from "../stores/movieStore";
+import singleStore from "../stores/singleStore";
 
 function MovieDetails() {
-	const { movieSlug } = useParams();
-	const movie = movieStore.movies.find((movie) => movie.slug === movieSlug);
-	if (!movie) return <Navigate to="/movies" />;
+  const { movieSlug } = useParams();
+  // const movie = movieStore.movies.find((movie) => movie.slug === movieSlug);
+  const movie = singleStore.movies.find((movie) => movie.slug === movieSlug);
 
-	const genresList = movie.genres.map((genre) => (
-		<Tags key={genre._id} item={genre.name} />
-	));
-	const celebritiesList = movie.celebrities.map((celebrity) => (
-		<Tags key={celebrity._id} item={celebrity.name} />
-	));
+  if (!movie) return <Navigate to="/movies" />;
 
-	return (
-		<div>
-			<div>
-				<img src={movie.image} alt="showing the movie" />
-				<h1>{movie.title}</h1>
-				<p>{movie.releaseDate}</p>
-				<span>{genresList}</span>
-				{celebritiesList}
-			</div>
-		</div>
-	);
+  const genresList = movie.genres.map((genre) => (
+    <Tags key={genre._id} item={genre.name} />
+  ));
+  const celebritiesList = movie.celebrities.map((celebrity) => (
+    <Tags key={celebrity._id} item={celebrity.name} />
+  ));
+
+  return (
+    <div>
+      <div>
+        <img src={movie.image} alt="showing the movie" />
+        <h1>{movie.title}</h1>
+        <p>{movie.releaseDate}</p>
+        <span>{genresList}</span>
+        {celebritiesList}
+      </div>
+    </div>
+  );
 }
 
 export default observer(MovieDetails);
