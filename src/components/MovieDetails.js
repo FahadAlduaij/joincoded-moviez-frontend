7293cc83de6bdaf9f417ca72ea23c033bd26e5b3
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-11-10 18:13:54 +0300

    :)

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -10,28 +10,28 @@ import Tags from "./Tags";
 import movieStore from "../stores/movieStore";
 
 function MovieDetails() {
-	const { movieSlug } = useParams();
-	const movie = movieStore.movies.find((movie) => movie.slug === movieSlug);
-	if (!movie) return <Navigate to="/movies" />;
+  const { movieSlug } = useParams();
+  const movie = movieStore.movies.find((movie) => movie.slug === movieSlug);
+  if (!movie) return <Navigate to="/movies" />;
 
-	const genresList = movie.genres.map((genre) => (
-		<Tags key={genre._id} item={genre.name} />
-	));
-	const celebritiesList = movie.celebrities.map((celebrity) => (
-		<Tags key={celebrity._id} item={celebrity.name} />
-	));
+  const genresList = movie.genres.map((genre) => (
+    <Tags key={genre._id} item={genre.name} />
+  ));
+  const celebritiesList = movie.celebrities.map((celebrity) => (
+    <Tags key={celebrity._id} item={celebrity.name} />
+  ));
 
-	return (
-		<div>
-			<div>
-				<img src={movie.image} alt="showing the movie" />
-				<h1>{movie.title}</h1>
-				<p>{movie.releaseDate}</p>
-				<span>{genresList}</span>
-				{celebritiesList}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <img className="imged" src={movie.image} alt="showing the movie" />
+        <h1 className="titled">{movie.title}</h1>
+        <p className="rdd">{movie.releaseDate}</p>
+        <span className="genbtn">{genresList}</span>
+        {celebritiesList}
+      </div>
+    </div>
+  );
 }
 
 export default observer(MovieDetails);
--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Tags({ item }) {
-	return (
-		<div>
-			<Link to="/">
-				<p variant="link">{item} </p>
-			</Link>
-		</div>
-	);
+  return (
+    <div>
+      <Link to="/">
+        <p>{item} </p>
+      </Link>
+    </div>
+  );
 }
 
 export default Tags;
