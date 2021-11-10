dcc7a2888c16d3c69a46220686c6827ec21bab97
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-11-10 11:03:53 +0300

    -

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 function SearchBar({ className, setItem }) {
-	return (
-		<div>
-			<div className={className}>
-				<form>
-					<img
-						className="searchimg"
-						src="http://www.clker.com/cliparts/R/6/l/K/8/R/search-icon-red-md.png"
-						alt="a logo for searching"
-					/>
-					<input
-						type="text"
-						placeholder=" Search for a movie"
-						onChange={(e) => setItem(e.target.value)}
-					/>
-				</form>
-			</div>
+  return (
+    <div>
+      <div className={className}>
+        <form>
+          <img
+            className="searchimg"
+            src="http://www.clker.com/cliparts/R/6/l/K/8/R/search-icon-red-md.png"
+            alt="a logo for searching"
+          />
+          <input
+            type="text"
+            placeholder=" Search for a movie"
+            onChange={(e) => setItem(e.target.value)}
+          />
+        </form>
+      </div>
 
-			{/* <div className="wrap position-relative ">
+      {/* <div className="wrap position-relative ">
 				<div className="search">
 					<img
 						className="searchimg"
@@ -33,8 +33,8 @@ function SearchBar({ className, setItem }) {
 					/>
 				</div>
 			</div> */}
-		</div>
-	);
+    </div>
+  );
 }
 
 export default SearchBar;
