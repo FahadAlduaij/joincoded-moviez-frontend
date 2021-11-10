5616b7e981b5ce7114ada7beb3cd674a9bd42c82
Alkandari-Y <yayalkandari@gmail.com>
2021-11-11 02:30:53 +0300

    meh

--- src/components/MoviesGenre.js
+++ src/components/MoviesGenre.js
@@ -1,20 +1,29 @@
 import { observer } from "mobx-react";
 import React from "react";
 import MovieCard from "./MovieCard";
+import { Link } from "react-router-dom";
 
 function MoviesGenre({ genre, query }) {
-	const movies = genre.movies
-		.filter((movie) => movie.title.toLowerCase().includes(query.toLowerCase()))
-		.map((movie) => <MovieCard key={movie._id} movie={movie} />);
+  const movies = genre.movies
+    .filter(
+      (movie) =>
+        movie.title.toLowerCase().includes(query.toLowerCase()) ||
+        movie.celebrities.filter((celebrity) =>
+          celebrity.name.toLowerCase().includes(query.toLowerCase())
+        )
+    )
+    .map((movie) => <MovieCard key={movie._id} movie={movie} />);
 
-	return (
-		<div>
-			<h1 className="genre-header">{genre.name}</h1>
-			<div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
-				{movies}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Link className="genre-header-links" to={`/genres/${genre.slug}`}>
+        <h1>{genre.name}</h1>
+      </Link>
+      <div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
+        {movies}
+      </div>
+    </div>
+  );
 }
 
 export default observer(MoviesGenre);
