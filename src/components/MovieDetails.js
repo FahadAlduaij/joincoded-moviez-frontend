534da51a4e6f9d697c2c06ba9496969b94493ac4
Alkandari-Y <yayalkandari@gmail.com>
2021-11-11 00:29:16 +0300

    comments merged into detail page

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,46 +5,59 @@ import { Navigate } from "react-router-dom";
 
 // Components
 import Tags from "./Tags";
+import MovieCommentInput from "./MovieCommentInput";
+import MovieComments from "./MovieComments";
 
 // stores
 // import movieStore from "../stores/movieStore";
 import singleStore from "../stores/singleStore";
 
 function MovieDetails() {
-	const { movieSlug } = useParams();
-	const movie = singleStore.movies.find((movie) => movie.slug === movieSlug);
-	if (!movie) return <Navigate to="/movies" />;
+  const { movieSlug } = useParams();
+  const movie = singleStore.movies.find((movie) => movie.slug === movieSlug);
+  if (!movie) return <Navigate to="/movies" />;
 
-	const genresList = movie.genres.map((genre) => (
-		<Tags
-			key={genre._id}
-			item={genre.name}
-			slug={genre.slug}
-			baseUrl="genres"
-		/>
-	));
-	const celebritiesList = movie.celebrities.map((celebrity) => (
-		<Tags
-			key={celebrity._id}
-			item={celebrity.name}
-			slug={celebrity.slug}
-			baseUrl="celebrities"
-		/>
-	));
+  const genresList = movie.genres.map((genre) => (
+    <Tags
+      key={genre._id}
+      item={genre.name}
+      slug={genre.slug}
+      baseUrl="genres"
+    />
+  ));
+  const celebritiesList = movie.celebrities.map((celebrity) => (
+    <Tags
+      key={celebrity._id}
+      item={celebrity.name}
+      slug={celebrity.slug}
+      baseUrl="celebrities"
+    />
+  ));
 
-	return (
-		<div className="container d-flex flex-row justify-content-start align-items-center p-2">
-			<img className="movieDetails-img" src={movie.image} alt="showing the movie" />
+  return (
+    <div>
+      <div className="container d-flex flex-row justify-content-start align-items-center p-2">
+        <img
+          className="movieDetails-img"
+          src={movie.image}
+          alt="showing the movie"
+        />
 
-			<div className="movieDetails-info flex-column flex-fill">
-				<h1 className="">{movie.title}</h1>
-				<p className="">{movie.releaseDate}</p>
-				<p className="">{movie.description}</p>
-				<div >{genresList}</div>
-				<div>{celebritiesList}</div>
-			</div>
-		</div>
-	);
+        <div className="movieDetails-info flex-column flex-fill">
+          <h1 className="">{movie.title}</h1>
+          <p className="">{movie.releaseDate}</p>
+          <p className="">{movie.description}</p>
+          <div>{genresList}</div>
+          <div>{celebritiesList}</div>
+        </div>
+      </div>
+      <div>
+        <MovieCommentInput movieId={movie._id} />
+
+        <MovieComments movieId={movie._id} />
+      </div>
+    </div>
+  );
 }
 
 export default observer(MovieDetails);
