40f102536444b92693eac6cf53a3f5228027f962
fahadalduaij <fahad_osama@hotmail.com>
2021-11-11 16:30:28 +0300

    edit cele name in Tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Tags({ item, slug, baseUrl }) {
 	return (
-		<div className="d-inline-flex flex-fill p-2 ">
+		<div className="d-inline-flex flex-fill flex-wrap p-2 ">
 			<Link to={`/${baseUrl}/${slug}`}>
 				<button className="genres-and-celebrities-tags">{item} </button>
 			</Link>
