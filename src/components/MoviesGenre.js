7e0096a612b2183c93adea9fb0c6a5bf9cc484b2
fahadalduaij <fahad_osama@hotmail.com>
2021-11-13 05:40:03 +0300

    fix github cuz  forget to pull

--- src/components/MoviesGenre.js
+++ src/components/MoviesGenre.js
@@ -10,7 +10,6 @@ function MoviesGenre({ genre, query }) {
 		.filter((movie) => movie.title.toLowerCase().includes(query.toLowerCase()))
 		.map((movie) => <MovieCard key={movie._id} movie={movie} />);
 
-<<<<<<< HEAD
 	if (movies.length >= 1) {
 		return (
 			<div>
@@ -29,27 +28,6 @@ function MoviesGenre({ genre, query }) {
 			</>
 		);
 	}
-=======
-    if (movies.length >= 1){
-  return (
-    
-    <div>
-      <Link className="genre-header-links" to={`/genres/${genre.slug}`}>
-        <h1>{genre.name}</h1>
-      </Link>
-      <div className="scrolling-wrapper row flex-row flex-nowrap pb-1 pt-2">
-        {movies}
-      </div>
-    </div>
-  );
-  } else {
-    return(    
-      <>
-      </>
-    )
-
-  }
->>>>>>> d856de1b33665243d94ebf22312e48faecf4596d
 }
 
 export default observer(MoviesGenre);
